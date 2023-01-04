76175c7b3ab90a13bb7a12cc5e6d93af31a35632
lonebots <jishnusurajila@gmail.com>
2023-01-05 00:05:17 +0530

    feat: Homepage added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import UserCard from '../components/UserCard'
+
+function HomePage() {
+    const user = {
+
+    }
+
+
+    return (
+        <div className='homepage'>
+            <div className='card-container'>
+                <div className=''>
+                    homepage prompt
+                    <UserCard user={user} />
+                </div>
+            </div>
+            <div className='button-container'>
+                <button className='button'>
+                    1
+                </button>
+                <button className='button'>
+                    2
+                </button>
+                <button className='button'>
+                    3
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
