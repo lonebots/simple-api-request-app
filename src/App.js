ecb4fceb5f56cbf8143a43025f48e433dbb3cde3
lonebots <jishnusurajila@gmail.com>
2023-01-05 14:44:08 +0530

    fix: removed unwanted react boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
 import HomePage from './pages/HomePage';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { getAllUsers } from './redux/User/user.actions';
 
 function App() {
-
-  // interact with the redux store
-  const [data, setData] = useState();
-
-  // loading app the call the api : https://reqres.in/api/users?
+  // loading app data with api call
   useEffect(
     () => {
       getAllUsers(1);
     }, []//empty dependencies for component did mount
   )
-
-
   return (
     <div className="App">
       <HomePage />
