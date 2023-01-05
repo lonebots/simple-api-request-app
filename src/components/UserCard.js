de5e7b9fde334f0490ae24ee5601e8da47587197
lonebots <jishnusurajila@gmail.com>
2023-01-05 13:52:22 +0530

    fix : user card console log removed

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,28 +3,28 @@ import { useSelector } from 'react-redux'
 
 const UserCard = ({ user }) => {
   const loading = useSelector(state => state.users.loading)
-  return (<>
-    {console.log("loading : ", loading)}
-    {
-      loading ?
-        (<div className='user-card'>loading...</div>) :
-        (
-          <div className='user-card' >
-            <div className='user-details'>
-              <div className='user-info'>
-                {user.first_name}
-                <p className='userdetail_text'> First Name: <b> {user.first_name} </b></p>
-                <p className='userdetail_text'> Last Name: <b> {user.last_name} </b></p>
-                <p className='userdetail_text'> Email Address: <b> {user.email}</b> </p>
-                <p className='userdetail_text'> User ID: <b> {user.id}</b> </p>
+  return (
+    <>
+      {
+        loading ?
+          (<div className='user-card'>loading...</div>) :
+          (
+            <div className='user-card' >
+              <div className='user-details'>
+                <div className='user-info'>
+                  {user.first_name}
+                  <p className='userdetail_text'> First Name: <b> {user.first_name} </b></p>
+                  <p className='userdetail_text'> Last Name: <b> {user.last_name} </b></p>
+                  <p className='userdetail_text'> Email Address: <b> {user.email}</b> </p>
+                  <p className='userdetail_text'> User ID: <b> {user.id}</b> </p>
+                </div>
+                <div className='user-avatar'>
+                  <img className='avatar' src={user.avatar} alt='user-avatar' />
+                </div>
               </div>
-              <div className='user-avatar'>
-                <img className='avatar' src={user.avatar} alt='user-avatar' />
-              </div>
-            </div>
-          </div >)
-    }
-  </>
+            </div >)
+      }
+    </>
 
   )
 }
